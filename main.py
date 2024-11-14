4e5c8e69b89cd6182749cbdd5b337b1ac9e6cc33
disclose-tech <152188727+disclose-tech@users.noreply.github.com>
2024-11-14 14:10:25 +0100

    Add run_name input

--- main.py
+++ main.py
@@ -80,6 +80,8 @@ class DiscloseIGEDDScraper(AddOn):
 
         # Add-on inputs
 
+        self.run_name = self.data.get("run_name", "no name")
+
         self.access_level = self.data["access_level"]
 
         self.target_year = self.data.get(
@@ -121,6 +123,7 @@ class DiscloseIGEDDScraper(AddOn):
             access_level=self.access_level,
             dry_run=self.dry_run,
             run_id=self.id,
+            run_name=self.run_name,
             send_mail=self.send_mail,
             load_event_data=self.load_event_data,
             store_event_data=self.store_event_data,
@@ -128,7 +131,9 @@ class DiscloseIGEDDScraper(AddOn):
 
         # Run
 
-        self.set_message(f"Scraping IGEDD documents ({str(self.target_year)})")
+        self.set_message(
+            f"Scraping IGEDD documents {str(self.target_year)} [{self.run_name}]"
+        )
         process.start()
         self.set_message("Scraping complete!")
 
